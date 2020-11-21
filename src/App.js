5d324c9ac81290589aab30001d3b4adfdf9f2f9a
Abdessamad <abdsamad23bnsaad@gmail.com>
2020-11-21 11:06:20 +0100

    change stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
+import Widgets from './Widgets';
 function App() {
   return (
     <div className="app">
-      <h1>Twitter Clone 🚀</h1>
-
       {/* Sidebar */}
-
+      
       <Sidebar />
 
       {/* Feed */}
 
+      <Feed />
 
       {/* Widgets */}
+
+      <Widgets />
     </div>
   );
 }
